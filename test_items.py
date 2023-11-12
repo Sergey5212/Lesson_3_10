a7ecda4eed45eedc66842924bd77c72b251fd634
Sergey5212 <sergey5212@gmail.com>
2023-11-12 19:05:13 +0300

    Fix link

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
 
-link = "https://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_add_to_basket_button_languages(browser):
